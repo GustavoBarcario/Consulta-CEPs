4e17110a0b770841dddec8987b31d5913f59d432
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2022-12-04 02:25:10 -0300

    Cria projeto Consulta CEPs

---
+++ Consulta CEP/FrmConsultarCEPS.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consulta_CEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(mskCEP.Text))
+            {
+                using (var ws = new WSCorreios.AtendeClienteClient())
+                {
+                    try
+                    {
+                        var endereco = ws.consultaCEP(mskCEP.Text.Trim());
+                        txtBairro.Text = endereco.bairro;
+                        txtCidade.Text = endereco.cidade;
+                        txtEstado.Text = endereco.uf;
+                        txtRua.Text = endereco.end;
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe um CEP válido!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            mskCEP.Focus();
+            mskCEP.Text = string.Empty;
+            txtEstado.Text= string.Empty;
+            txtBairro.Text = string.Empty;
+            txtCidade.Text = string.Empty;
+            txtRua.Text = string.Empty;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mskCEP.Select();
+        }
+    }
+}
